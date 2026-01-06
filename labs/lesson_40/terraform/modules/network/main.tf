c3bad43059f4ed04953a09bd02c72200dbbf5cbb
VlrRbn <ysukata@gmail.com>
2026-01-06 19:26:40 +0000

    second push

--- labs/lesson_40/terraform/modules/network/main.tf
+++ labs/lesson_40/terraform/modules/network/main.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = "~> 1.14.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
 }
 
 data "aws_availability_zones" "available" {
